a049e065451e25a10673330abfbd3d89acbea9ec
Danny <ndq880815@hotmail.com>
2016-07-09 19:00:48 +0800

    Correct wrong spelling of merchant in source code

--- PictoLottery/src/main/java/com/picto/controller/front/StartLotteryController.java
+++ PictoLottery/src/main/java/com/picto/controller/front/StartLotteryController.java
@@ -1,27 +1,20 @@
 package com.picto.controller.front;
 
-import com.picto.constants.Constants;
+
 import com.picto.dao.MerchantDao;
 import com.picto.entity.Merchant;
-import com.picto.service.StartLotteryService;
-import com.picto.util.WechatUtil;
-import org.apache.commons.logging.Log;
+
 import org.apache.log4j.Logger;
-import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
 
 /**
  * Created by wujigang on 2016/5/22.
@@ -31,13 +24,13 @@ public class StartLotteryController {
     private static final Logger logger = Logger.getLogger(StartLotteryController.class);
 
     @Autowired
-    private MerchantDao mechantDao;
+    private MerchantDao merchantDao;
 
     @RequestMapping(value = "/startLottery", method = RequestMethod.GET)
     public String startLottery(@RequestParam("merchantId") Integer merchantId, @RequestParam("code") String code,
                                Model model, HttpServletRequest request) {
         logger.info("进入买单先生页面merchantId=" + merchantId + ",code=" + code);
-        Merchant merchant = mechantDao.queryMerchantById(merchantId);
+        Merchant merchant = merchantDao.queryMerchantById(merchantId);
 
 //      model.addAttribute("merchant", merchant);
         model.addAttribute("merchantId", merchantId);
