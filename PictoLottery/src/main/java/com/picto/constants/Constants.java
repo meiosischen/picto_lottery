4079c712394d0b5892f2833145a71fd1304794ea
SSI <SSI@SSI-PC>
2016-07-20 22:09:18 +0800

    Change environment to "production"

--- PictoLottery/src/main/java/com/picto/constants/Constants.java
+++ PictoLottery/src/main/java/com/picto/constants/Constants.java
@@ -7,8 +7,9 @@ public interface Constants {
     int OPERATION_TYPE_LOTTERY = 1;//抽奖
     int OPERATION_TYPE_EXCHANGED = 2;//兑换
 
-    String ENV_DEV = "dev";//测试环境
-
+    //String ENV_DEV = "dev";//测试环境
+    String ENV_DEV = "production";//测试环境
+    
     int COUPON_STATE_EFFECTED = 1;
     int COUPON_STATE_EXCHANGED = 2;
 
